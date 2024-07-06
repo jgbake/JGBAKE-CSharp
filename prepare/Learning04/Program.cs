1d5676ec480656246f750f140884c4ec6f468e06
Jaxon Bake <jaxongbake@gmail.com>
2024-07-05 22:08:23 -0600

    Learn 04 - Homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignTest = new Assignment("Jonathan Barnet", "Quantum Mechanics");
+        Console.WriteLine(assignTest.GetSummary());
+
+        MathAssignment mathAssignTest = new MathAssignment("Test Name", "Some Topic", "8.9", "31-77");
+        Console.WriteLine($"\n{mathAssignTest.GetSummary()}");
+        Console.WriteLine(mathAssignTest.GetHomeworkList());
+
+        WritingAssignment writeAssignTest = new WritingAssignment("Write Test", "Some Writing Topic", "Some Cool Paper Title");
+        Console.WriteLine($"\n{writeAssignTest.GetSummary()}");
+        Console.WriteLine(writeAssignTest.GetWritingInformation());
     }
 }
