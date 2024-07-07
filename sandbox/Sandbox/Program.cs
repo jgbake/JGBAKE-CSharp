6d70503bef72e464c5c34e57fc234f3c7da2fac3
Jaxon Bake <jaxongbake@gmail.com>
2024-07-06 18:48:09 -0600

    DotDotDot function

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,20 @@
 using System;
-
 class Program
 {
+    static void DotDotDot()
+    {
+        Console.Write(Convert.ToChar(176));
+        Thread.Sleep(800);
+        Console.Write(Convert.ToChar(176));
+        Thread.Sleep(800);
+        Console.Write(Convert.ToChar(176));
+        Thread.Sleep(800);
+        Console.Write("\b\b\b   \b\b\b");
+        Thread.Sleep(800);
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        DotDotDot();
     }
 }
