71b0acddba50ea821c6c84ae54ea4ccfc2a95925
Jaxon Bake <jaxongbake@gmail.com>
2024-07-11 17:49:12 -0600

    Learning 05 - Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [
+            new Square("Red", 13),
+            new Rectangle("Green", 4.5, 17),
+            new Circle("Blue", 7.5) ];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The shape is {shape.GetColor()}, and its area is {shape.GetArea()}.");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,22 @@
+using System;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string newColor)
+    {
+        _color = newColor;
+    }
+
+    public abstract double GetArea();
+}
